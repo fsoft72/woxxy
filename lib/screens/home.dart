d8cc533cca96dc0afe5f4231b0ea4e6d5f1b379a
Fabio Rotondo <fabio.rotondo@gmail.com>
2025-02-20 13:26:05 +0100

    chore: remove pubspec.lock file

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,25 +3,51 @@ import '../services/network_service.dart';
 import '../models/peer.dart';
 import 'peer_details.dart';
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
   final NetworkService networkService;
-
   const HomeContent({
     super.key,
     required this.networkService,
   });
 
   @override
+  State<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  @override
+  void initState() {
+    super.initState();
+    widget.networkService.fileReceived.listen((fileInfo) {
+      if (!mounted) return;
+      final parts = fileInfo.split('|');
+      if (parts.length >= 4) {
+        final sizeMiB = parts[1];
+        final transferTime = parts[2];
+        final speed = parts[3];
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'File received successfully ($sizeMiB MiB in ${transferTime}s, $speed MiB/s)',
+            ),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Peer>>(
-      stream: networkService.peerStream,
+      stream: widget.networkService.peerStream,
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return const Center(
             child: Text('No peers found. Searching...'),
           );
         }
-        final peers = snapshot.data!.where((peer) => peer.address.address != networkService.currentIpAddress).toList();
+        final peers = snapshot.data!.where((peer) => peer.address.address != widget.networkService.currentIpAddress).toList();
         if (peers.isEmpty) {
           return const Center(
             child: Text('No other peers found on the network'),
@@ -40,7 +66,7 @@ class HomeContent extends StatelessWidget {
                   MaterialPageRoute(
                     builder: (context) => PeerDetailPage(
                       peer: peer,
-                      networkService: networkService,
+                      networkService: widget.networkService,
                     ),
                   ),
                 );
