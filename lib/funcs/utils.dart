b83d97894a764abbdc0f850bfcea1052acfe39e8
Fabio Rotondo <fabio.rotondo@gmail.com>
2025-03-05 06:30:37 +0100

    ADD: implement openFileLocation function to open file directories

--- lib/funcs/utils.dart
+++ lib/funcs/utils.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:url_launcher/url_launcher.dart';
 
 void showSnackbar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -18,4 +21,16 @@ String generateTransferId(String filename) {
   String s = '${filename}_${date}';
 
   return md5.convert(utf8.encode(s)).toString();
+}
+
+Future<void> openFileLocation(String filePath) async {
+  final file = File(filePath);
+
+  if (Platform.isWindows) {
+    // Use Process.run instead of url_launcher for Windows
+    await Process.run('explorer.exe', ['/select,', file.path]);
+  } else {
+    Uri uri = Uri.parse('file://${path.dirname(file.path)}');
+    await launchUrl(uri);
+  }
 }
