eb3d2698ed73694ed3083c8a80d897c78fcaeb29
Fabio Rotondo <fabio.rotondo@gmail.com>
2025-03-12 07:37:22 +0100

    REFINE: restrict zprint to debug mode only

--- lib/funcs/debug.dart
+++ lib/funcs/debug.dart
@@ -1,5 +1,11 @@
 // ignore_for_file: avoid_print
 
 void zprint(String message) {
+  // print only in debug mode
+
+  if (const bool.fromEnvironment('dart.vm.product')) {
+    return;
+  }
+
   print("[Woxxy] $message");
 }
